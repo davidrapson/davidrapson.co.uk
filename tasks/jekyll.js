c83b3c4635e42ba5da05ecd4fb45d2e1fa17e259
David Rapson <david@davidrapson.co.uk>
2015-06-28 22:35:39 +0100

    Use bundle exec for Jekyll

--- tasks/jekyll.js
+++ tasks/jekyll.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 
 gulp.task('jekyll', function(done) {
-    exec('jekyll build --drafts --future --config _config.yml,_config-dev.yml', done);
+    exec('bundle exec jekyll build --drafts --future --config _config.yml,_config-dev.yml', done);
 });
 
 gulp.task('jekyll:production', function(done) {
-    exec('jekyll build', done);
+    exec('bundle exec jekyll build', done);
 });
