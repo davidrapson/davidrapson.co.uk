46d9dc0d9f215a120efbdc664a0e60097bf12619
David Rapson <david@davidrapson.co.uk>
2015-06-28 22:37:48 +0100

    pagespeed api update

--- tasks/perf.js
+++ tasks/perf.js
@@ -2,8 +2,7 @@ var gulp = require('gulp');
 var pagespeed = require('psi');
 
 gulp.task('pagespeed', function () {
-    pagespeed({
-        url: 'http://davidrapson.co.uk/',
+    pagespeed.output('http://davidrapson.co.uk/', {
         strategy: 'mobile'
     });
 });
