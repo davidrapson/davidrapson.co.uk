0975cccd11eae4a7a96e4491b281b5f069b9f8be
David Rapson <david@davidrapson.co.uk>
2015-07-26 21:09:18 +0100

    Try logging user timing in GA, allow for dev account

---
+++ source/_includes/userTimingRum.js
@@ -0,0 +1,31 @@
+// Support routines for automatically reporting user timing for common analytics platforms
+(function() {
+
+    if(!(
+        'performance' in window &&
+        'addEventListener' in window &&
+        typeof Array.prototype.forEach !== 'undefined'
+    )) {
+        return;
+    }
+
+    window.addEventListener('load', function() {
+        var marks = window.performance.getEntriesByType("mark");
+        var logMarks = {};
+        marks.forEach(function(mark) {
+            var time = Math.round(mark.startTime);
+            var splitName = mark.name.split('.');
+            if (time >= 0 && time < 3600000 && window.ga) {
+                ga('send', {
+                  'hitType': 'timing',
+                  'timingCategory': 'mark',
+                  'timingVar': splitName[0],
+                  'timingValue': time,
+                  'timingLabel': splitName[1],
+                  'page': window.location.pathname
+                });
+            }
+        });
+    }, false);
+
+})();
