37d0b708c22e2bf7e81c3410f60922fe210a0222
David Rapson <david@davidrapson.co.uk>
2015-07-26 16:38:31 +0100

    Update to picturefill 3 beta

--- tasks/js.js
+++ tasks/js.js
@@ -5,8 +5,7 @@ var pkg = require('../package.json');
 
 gulp.task('js', ['jshint'], function () {
     return gulp.src([
-        paths.jsSrc + '/components/picturefill/dist/picturefill.min.js',
-        paths.jsSrc + '/components/lazysizes/lazysizes.min.js'
+        paths.jsSrc + '/bower_components/picturefill/dist/picturefill.min.js'
     ])
         .pipe(plugins.plumber())
         .pipe(plugins.sourcemaps.init())
