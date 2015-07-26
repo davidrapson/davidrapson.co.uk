6e3057a49a5bc0ce7586f04c1ea68b028ea6c18e
David Rapson <david@davidrapson.co.uk>
2015-07-26 22:48:58 +0100

    Run clean before building

--- tasks/jekyll.js
+++ tasks/jekyll.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 
 gulp.task('jekyll', function(done) {
+    exec('bundle exec jekyll clean');
     exec('bundle exec jekyll build --drafts --future --config _config.yml,_config-dev.yml', done);
 });
 
 gulp.task('jekyll:production', function(done) {
+    exec('bundle exec jekyll clean');
     exec('bundle exec jekyll build', done);
 });
