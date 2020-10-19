141cadd6373e82e4998de87de6ad835fa81a634b
David Rapson <david@davidrapson.co.uk>
2020-10-19 20:10:58 +0100

    Cleanup and improve typography

--- .eleventy.js
+++ .eleventy.js
@@ -2,23 +2,23 @@ const del = require("del");
 const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
-    const dir = {
-        input: "src",
-        output: "_site",
-    };
+  const dir = {
+    input: "src",
+    output: "_site",
+  };
 
-    // Clean output directory on build
-    del(`${dir.output}/*`);
+  // Clean output directory on build
+  del(`${dir.output}/*`);
 
-    // Passthrough copy all images
-    eleventyConfig.addPassthroughCopy({
-        [`${dir.input}/*.{png,ico,jpg}`]: ".",
-    });
+  // Passthrough copy all images
+  eleventyConfig.addPassthroughCopy({
+    [`${dir.input}/*.{png,ico,jpg}`]: ".",
+  });
 
-    // Minify CSS filter, used to inline all styles
-    eleventyConfig.addFilter("cssmin", function (code) {
-        return new CleanCSS({}).minify(code).styles;
-    });
+  // Minify CSS filter, used to inline all styles
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
-    return { dir };
+  return { dir };
 };
