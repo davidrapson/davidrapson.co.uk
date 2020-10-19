821aa0d8bca1a4ea85f10e702359865afc68843a
David Rapson <david@davidrapson.co.uk>
2020-10-19 19:27:44 +0100

    Configure eleventy (#86)
    * Configure eleventy
    * Extract layout and css
    * Add prettier
    * Move source files to src

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const CleanCSS = require("clean-css");
+
+module.exports = function (eleventyConfig) {
+    // Find and copy any image files to _site
+    // Does not maintain directory structure.
+    eleventyConfig.addPassthroughCopy({ "*.{png,ico,jpg}": "." });
+
+    eleventyConfig.addFilter("cssmin", function (code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "_site",
+        },
+    };
+};
