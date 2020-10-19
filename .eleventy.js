19a30ae56a011a4ebf563ddd4e5be46a28f7ab42
David Rapson <david@davidrapson.co.uk>
2020-10-19 19:51:21 +0100

    Fixup image path

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const CleanCSS = require("clean-css");
 module.exports = function (eleventyConfig) {
     // Find and copy any image files to _site
     // Does not maintain directory structure.
-    eleventyConfig.addPassthroughCopy({ "*.{png,ico,jpg}": "." });
+    eleventyConfig.addPassthroughCopy({ "src/*.{png,ico,jpg}": "." });
 
     eleventyConfig.addFilter("cssmin", function (code) {
         return new CleanCSS({}).minify(code).styles;
