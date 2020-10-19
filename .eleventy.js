70ba1f594dce64af230622aeb51ba6c87b974737
David Rapson <david@davidrapson.co.uk>
2020-10-19 19:08:36 +0100

    Extract layout and css

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function (eleventyConfig) {
     // Find and copy any image files to _site
     // Does not maintain directory structure.
     eleventyConfig.addPassthroughCopy({ "*.{png,ico,jpg}": "." });
+
+    eleventyConfig.addFilter("cssmin", function (code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
 };
